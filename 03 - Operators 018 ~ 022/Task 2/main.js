54f637898787bc7a7ab280557abe08e24197c452
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-06-28 20:23:46 +0300

    Add Assignment Two In Topic Three

---
+++ 03 - Operators 018 ~ 022/Task 2/main.js
@@ -0,0 +1,19 @@
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num + num + (num * false)); // 6
+
+// Soultion Three
+console.log((typeof num).length); // 6
+
+// Soultion Four
+console.log(num * ( true + true)); // 6
+
+// Solution Five
+console.log(num / num + num + (num - true)); // 6
+
+// Solution Six
+console.log(num ** (num - true) - num); // 6
