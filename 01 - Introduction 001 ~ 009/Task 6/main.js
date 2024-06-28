e7b038190949cca3b74d8e7bebb66c1f1d2df5cd
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-06-28 20:20:31 +0300

    Add Assignment Six In Topic One

---
+++ 01 - Introduction 001 ~ 009/Task 6/main.js
@@ -0,0 +1,10 @@
+// ! Approach 1
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+
+/*
+  ! Approach 2
+  console.log("Iam In Console");
+  document.write("Iam In Page");
+*/
