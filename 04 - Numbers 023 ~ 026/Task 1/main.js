92a975652666f264ae5efd6ada300d8b52e26b22
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-08-11 23:46:14 +0300

    Add Assignment One In Topic Four

---
+++ 04 - Numbers 023 ~ 026/Task 1/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(Number("100000")); // 100000
+console.log(parseInt(100000)); // 100000
+console.log(parseFloat(100000)); // 100000
+console.log(Math.trunc(100000.23487)); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(Math.ceil(99999.245435)); // 100000
+console.log(Math.floor(100000.7546)); // 100000
+console.log(Math.round(100000.234231)); // 100000
+console.log(+"100000"); // 100000
+console.log(+`${100 * 1000}`); // 100000
