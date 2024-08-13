fcc3a4843d7ba8a37f2da1f9ec3eb0a1a5e3a00c
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-08-13 06:47:50 +0300

    Add Assignment Two In Topic Thirteen

---
+++ 13 - Function And Scopes 064 ~ 070/Task 2/main.js
@@ -0,0 +1,7 @@
+// ! Function 1
+let itsMe = _ => `Iam A Normal Function`;
+console.log(itsMe()); // Iam A Normal Function
+
+// ! Function 2
+let urlCreate = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
+console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
