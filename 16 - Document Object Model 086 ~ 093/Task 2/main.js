692575e79fe4aec3458a30226c1933344aeb0857
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-08-17 02:47:08 +0300

    Add Assignment Two In Topic Sixteen

---
+++ 16 - Document Object Model 086 ~ 093/Task 2/main.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 10; i++) {
+  document.querySelectorAll("img")[i].src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+  document.querySelectorAll("img")[i].alt = "Elzero Logo";
+}
