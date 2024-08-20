68794ea5fcf95685958315f74a595733da853d04
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-08-20 07:05:19 +0300

    Add Assignment Two In Topic Twenty Five

---
+++ 25 - AJAX And JSON 169 ~ 178/Task 2/main.js
@@ -0,0 +1,14 @@
+let myReqest = new XMLHttpRequest();
+
+myReqest.open("GET", "../Task 1/articles.json");
+myReqest.send();
+
+myReqest.onreadystatechange = () => {
+    if (myReqest.readyState === 4 && myReqest.status === 200) {
+        console.log(myReqest.responseText);
+    }
+};
+
+myReqest.onloadend = function () {
+    console.log("Data Loaded");
+};
