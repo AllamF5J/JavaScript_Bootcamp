a9d6fe0d5cff5068def0e9d926e093c051bf4334
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-08-23 09:57:28 +0300

    Add Assignment Two In Topic Twenty Five

--- 25 - AJAX And JSON 169 ~ 178/Task 2/main.js
+++ 25 - AJAX And JSON 169 ~ 178/Task 2/main.js
@@ -4,11 +4,11 @@ myReqest.open("GET", "../Task 1/articles.json");
 myReqest.send();
 
 myReqest.onreadystatechange = () => {
-    if (myReqest.readyState === 4 && myReqest.status === 200) {
-        console.log(myReqest.responseText);
-    }
+  if (myReqest.readyState === 4 && myReqest.status === 200) {
+    console.log(myReqest.responseText);
+  }
 };
 
 myReqest.onloadend = function () {
-    console.log("Data Loaded");
+  console.log("Data Loaded");
 };
